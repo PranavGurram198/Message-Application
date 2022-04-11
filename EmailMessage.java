1c3be4aee71612befdb31bec79d637df4b6ce018
PranavGurram198 <101916625+PranavGurram198@users.noreply.github.com>
2022-04-11 23:02:33 +0530

    Add files via upload

---
+++ EmailMessage.java
@@ -0,0 +1,13 @@
+package messanger;
+
+public class EmailMessage {
+	
+	public String receiver, message;
+
+	public EmailMessage(String receiver, String message) 
+	 {
+	     this.receiver = receiver;
+	     this.message = message;
+	 }
+
+}
---
+++ SmsMessage.java
@@ -0,0 +1,13 @@
+package messanger;
+
+public class SmsMessage {
+	
+	public String receiver, message;
+
+	public SmsMessage(String receiver, String message)
+	{
+		this.receiver = receiver;
+		this.message = message;
+	}
+
+}
